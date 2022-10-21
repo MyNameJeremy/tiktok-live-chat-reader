df7e86957b3d19fa186bc20b81d4d49b79a917bc
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2022-10-22 00:17:26 +0200

    made it a chrome extension that console logs the chat

---
+++ scripts/content.js
@@ -0,0 +1,37 @@
+let messageContent;
+let messageAuthor;
+// let slayCounter = {};
+
+console.log(124);
+
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.addedNodes.length) {
+      messageAuthor = mutation.addedNodes[0].childNodes[1].childNodes[0].getElementsByClassName('tiktok-batvl-SpanNickName')[0].innerHTML;
+      messageContent = mutation.addedNodes[0].childNodes[1].childNodes[1].innerHTML;
+
+      // if (messageContent.toLowerCase().includes('slay')) {
+      //   if (!slayCounter.hasOwnProperty(messageAuthor)) {
+      //     slayCounter[messageAuthor] = 0;
+      //   }
+      //   slayCounter[messageAuthor] += (messageContent.toLowerCase().match(/slay/g) || []).length;
+      //   console.log(messageAuthor + "'s slay counter is now: " + slayCounter[messageAuthor]);
+      // }
+      if (messageAuthor && messageContent) {
+        console.log(messageAuthor + ' schreibt: ' + messageContent);
+      }
+    }
+  });
+});
+window.addEventListener('load', function () {
+  this.setTimeout(() => {
+    const chatWindow = document.getElementsByClassName('tiktok-1gwk1og-DivChatMessageList')[0];
+    console.log(chatWindow);
+
+    if (chatWindow) {
+      observer.observe(chatWindow, {
+        childList: true,
+      });
+    }
+  }, 1000);
+});
