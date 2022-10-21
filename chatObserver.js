98efdbc6b0761393b985e5116eae3a004abab4d9
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2022-10-21 20:38:35 +0200

    built a "slay counter" that counts how many times someone wrote the message "slay"

--- chatObserver.js
+++ chatObserver.js
@@ -1,15 +1,22 @@
 let messageContent = '';
 let messageAuthor = '';
-// let test;
+let slayCounter = {};
 
 const observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
     if (mutation.addedNodes.length) {
       messageAuthor = mutation.addedNodes[0].childNodes[1].childNodes[0].getElementsByClassName('tiktok-batvl-SpanNickName')[0].innerHTML;
       messageContent = mutation.addedNodes[0].childNodes[1].childNodes[1].innerHTML;
-      //   test = mutation.addedNodes[0].childNodes[1].childNodes[0];
 
-      console.log(messageAuthor + ' schreibt: ' + messageContent);
+      if (messageContent.toLowerCase() === 'slay') {
+        if (!slayCounter.hasOwnProperty(messageAuthor)) {
+          slayCounter[messageAuthor] = 0;
+        }
+        slayCounter[messageAuthor] += 1;
+        console.log(messageAuthor + "'s slay counter is now: " + slayCounter[messageAuthor]);
+      }
+
+      //   console.log(messageAuthor + ' schreibt: ' + messageContent);
     }
   });
 });
