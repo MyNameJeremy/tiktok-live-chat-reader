8be0a5764094b5366e908996b27780da31a96ecc
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2022-10-22 14:10:10 +0200

    added a button to trigger the comment display and watcher

--- scripts/content.js
+++ scripts/content.js
@@ -24,35 +24,38 @@ const observer = new MutationObserver(function (mutations) {
 });
 window.addEventListener('load', function () {
   this.setTimeout(() => {
-    const chatWindow = document.getElementsByClassName('tiktok-1gwk1og-DivChatMessageList')[0];
-    console.log(chatWindow);
-
-    let display = document.createElement('div');
-    display.innerHTML += '<style>table, td, th {border: 1px solid #fff;}table {width: 100%;border-collapse: collapse;}</style>';
-    display.style.zIndex = '1000';
-    display.style.position = 'fixed';
-    display.style.top = '0';
-    display.style.width = '100%';
-    display.style.height = '100%';
-    display.style.backgroundColor = '#232323';
-
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
-
-    thead.innerHTML = '<tr><th>Author</th><th>Message</th></tr>';
-    table.appendChild(thead);
-    tbody.setAttribute('id', 'messagesTableBody');
-    table.appendChild(tbody);
-    table.style.color = '#fff';
-    display.appendChild(table);
-
-    this.document.getElementsByTagName('body')[0].appendChild(display);
-
-    if (chatWindow) {
-      observer.observe(chatWindow, {
-        childList: true,
-      });
-    }
+    document.getElementsByClassName('tiktok-ba55d9-DivHeaderRightContainer')[0].innerHTML += '<button id="triggerDisplayAndWatcher">test</button>';
+    document.getElementById('triggerDisplayAndWatcher').addEventListener('click', function () {
+      triggerWatcherAndDisplay();
+    });
   }, 5000);
 });
+
+function triggerWatcherAndDisplay() {
+  const chatWindow = document.getElementsByClassName('tiktok-1gwk1og-DivChatMessageList')[0];
+  console.log(chatWindow);
+
+  let display = document.createElement('div');
+  display.setAttribute('id', 'display');
+  display.innerHTML +=
+    '<style>#display {z-index: 1000; position: fixed; top: 0; width: 100%; height: 100%; background-color: #232323;} table, td, th {border: 1px solid #fff;}table {width: 100%;border-collapse: collapse;}</style>';
+  let table = document.createElement('table');
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
+
+  thead.innerHTML = '<tr><th>Author</th><th>Message</th></tr>';
+  table.appendChild(thead);
+  tbody.setAttribute('id', 'messagesTableBody');
+  table.appendChild(tbody);
+  table.style.color = '#fff';
+  display.appendChild(table);
+
+  this.document.getElementsByTagName('body')[0].appendChild(display);
+
+  document.getElementsByClassName('vidiq-c-lesPJm')[0].remove();
+  if (chatWindow) {
+    observer.observe(chatWindow, {
+      childList: true,
+    });
+  }
+}
