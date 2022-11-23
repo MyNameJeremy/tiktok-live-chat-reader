94ca90d1990f0675727a1602af72bd620bea2fc5
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2022-11-23 16:46:08 +0100

    added reconnect button for the websocket

--- scripts/content.js
+++ scripts/content.js
@@ -28,6 +28,7 @@ fetch(ipFile)
         document.getElementById('triggerDisplayAndWatcher').addEventListener('click', function () {
           // triggerDisplay();
           addDonorOnlySwitch();
+          addReconnectButton();
           triggerWatcher();
         });
       }, 5000);
@@ -116,81 +117,141 @@ fetch(ipFile)
       scoreOption2value.innerHTML = score2;
     }
 
+    function addReconnectButton() {
+      let buttonHtmlAndCss = /*html*/ `
+        <style>
+          #reconnect-button{
+          width: 10em;
+          position: relative;
+          height: 3.5em;
+          border: 3px ridge #149CEA;
+          outline: none;
+          background-color: transparent;
+          color: white;
+          transition: 1s;
+          border-radius: 0.3em;
+          font-size: 16px;
+          font-weight: bold;
+          }
+
+          button::after {
+          content: "";
+          position: absolute;
+          top: -10px;
+          left: 3%;
+          width: 95%;
+          height: 40%;
+          background-color: #212121;
+          transition: 0.5s;
+          transform-origin: center;
+          }
+
+          #reconnect-button::before {
+          content: "";
+          transform-origin: center;
+          position: absolute;
+          top: 80%;
+          left: 3%;
+          width: 95%;
+          height: 40%;
+          background-color: #212121;
+          transition: 0.5s;
+          }
+
+          #reconnect-button:hover::before, button:hover::after {
+          transform: scale(0)
+          }
+
+          #reconnect-button:hover {
+          box-shadow: inset 0px 0px 25px #1479EA;
+          }
+        </style>
+        <button id="reconnect-button">
+          reconnect websocket
+        </button>
+      `;
+      document.getElementsByClassName('tiktok-kidgju-DivHeaderWrapperMain')[0].innerHTML += buttonHtmlAndCss;
+      document.getElementById('reconnect-button').addEventListener('click', function () {
+        webSocket.close();
+        webSocket = new WebSocket(`ws://${ipAddress}:8080`);
+      });
+    }
+
     function addDonorOnlySwitch() {
       let switchHTML = /*html*/ `
-  <style>
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 120px;
-    height: 34px;
-   }
-   
-   .switch input {
-    display: none;
-   }
-   
-   .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #3C3C3C;
-    -webkit-transition: .4s;
-    transition: .4s;
-    border-radius: 34px;
-   }
-   
-   .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    -webkit-transition: .4s;
-    transition: .4s;
-    border-radius: 50%;
-   }
-   
-   input:checked + .slider {
-    background-color: #0E6EB8;
-   }
-   
-   input:focus + .slider {
-    box-shadow: 0 0 1px #2196F3;
-   }
-   
-   input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(85px);
-   }
-   
-   /*------ ADDED CSS ---------*/
-   .slider:after {
-    content: 'DISABLED';
-    color: white;
-    display: block;
-    position: absolute;
-    transform: translate(-50%,-50%);
-    top: 50%;
-    left: 50%;
-    font-size: 10px;
-    font-family: Verdana, sans-serif;
-   }
-   
-   input:checked + .slider:after {
-    content: 'ENABLED';
-   }
-  </style>
-  <label class="switch">
-  <input id="donorOnlyMode" type="checkbox">
-  <span class="slider"></span>
-</label>
+        <style>
+          .switch {
+          position: relative;
+          display: inline-block;
+          width: 120px;
+          height: 34px;
+          }
+
+          .switch input {
+          display: none;
+          }
+
+          .slider {
+          position: absolute;
+          cursor: pointer;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: #3C3C3C;
+          -webkit-transition: .4s;
+          transition: .4s;
+          border-radius: 34px;
+          }
+
+          .slider:before {
+          position: absolute;
+          content: "";
+          height: 26px;
+          width: 26px;
+          left: 4px;
+          bottom: 4px;
+          background-color: white;
+          -webkit-transition: .4s;
+          transition: .4s;
+          border-radius: 50%;
+          }
+
+          input:checked + .slider {
+          background-color: #0E6EB8;
+          }
+
+          input:focus + .slider {
+          box-shadow: 0 0 1px #2196F3;
+          }
+
+          input:checked + .slider:before {
+          -webkit-transform: translateX(26px);
+          -ms-transform: translateX(26px);
+          transform: translateX(85px);
+          }
+
+          /*------ ADDED CSS ---------*/
+          .slider:after {
+          content: 'DISABLED';
+          color: white;
+          display: block;
+          position: absolute;
+          transform: translate(-50%,-50%);
+          top: 50%;
+          left: 50%;
+          font-size: 10px;
+          font-family: Verdana, sans-serif;
+          }
+
+          input:checked + .slider:after {
+          content: 'ENABLED';
+          }
+        </style>
+        <label class="switch">
+          <input id="donorOnlyMode" type="checkbox">
+          <span class="slider"></span>
+        </label>
   `;
       document.getElementsByClassName('tiktok-kidgju-DivHeaderWrapperMain')[0].innerHTML += switchHTML;
     }
